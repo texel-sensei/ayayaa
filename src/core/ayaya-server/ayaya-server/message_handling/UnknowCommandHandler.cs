001887f7d44b5ce29c51911a94db16b8fd768e2f
texel-sensei <texelsensei@posteo.net>
2018-03-17 16:03:16 +0100

    Unknown command handler now replies with the unknown command

--- src/core/ayaya-server/ayaya-server/message_handling/UnknowCommandHandler.cs
+++ src/core/ayaya-server/ayaya-server/message_handling/UnknowCommandHandler.cs
@@ -1,11 +1,14 @@
-﻿namespace ayaya_server.message_handling
+﻿using Newtonsoft.Json.Linq;
+
+namespace ayaya_server.message_handling
 {
     public class UnknowCommandHandler : IHandler
     {
         public Response HandleMessage(Packet data) => new Response()
         {
             Code = ResponseCode.UnknownCommand,
-            CodeText = $"Unknown command {data.Command}"
+            CodeText = $"Unknown command '{data.Command}'",
+            Data = new JObject {{ "Unknown", data.Command }}
         };
     }
 }
