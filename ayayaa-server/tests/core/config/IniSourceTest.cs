ac78ebde175bf8efa54b221d7a0c3a7b5783a53d
texel-sensei <texelsensei@posteo.net>
2018-09-16 16:47:22 +0200

    feat(config): Add Support for reading ini files
    A very simple config source that reads an *.ini file to provide config
    values.

---
+++ ayayaa-server/tests/core/config/IniSourceTest.cs
@@ -0,0 +1,32 @@
+﻿
+using ayayaa.core.config;
+using ayayaa.core.config.sources;
+using Xunit;
+
+namespace tests.core.config
+{
+    public class IniSourceTest
+    {
+        private readonly ConfigManager _manager = new ConfigManager();
+
+        [Fact]
+        private void TestIniConfig()
+        {
+            var configString =
+                @"
+[Test]
+SomeInt = 77
+SomeString = Hello World!
+; Some comment
+; SomeBool = true ; commented out, to test missing values
+";
+            _manager.AddConfigSource(IniConfigSource.FromString(configString));
+
+            var config = _manager.GetConfigObject<TestConfig>();
+
+            Assert.Equal(77, config.SomeInt);
+            Assert.Equal("Hello World!", config.SomeString);
+            Assert.False(config.SomeBool);
+        }
+    }
+}
